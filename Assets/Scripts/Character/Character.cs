3894a3991056a91f8a31f1c3f63ef66cfa057dc0
Tian Bornman <tianbornman.tb@gmail.com>
2025-10-14 19:49:08 +0200

    Adding models and basic animation

---
+++ Assets/Scripts/Character/Character.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+	// Editor variables
+	public Stats stats;
+}
---
+++ Assets/Scripts/Model/Stats.cs
@@ -0,0 +1,9 @@
+using System;
+
+[Serializable]
+public class Stats
+{
+	public float health;
+	public float maxHealth;
+	public float damage;
+}
