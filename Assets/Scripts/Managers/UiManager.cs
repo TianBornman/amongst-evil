376724f021017e48871eb69130930ceafcafc018
Tian Bornman <tianbornman.tb@gmail.com>
2025-10-16 09:10:58 +0200

    Show and Hide Stats

--- Assets/Scripts/Managers/UiManager.cs
+++ Assets/Scripts/Managers/UiManager.cs
@@ -1,8 +1,36 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.UIElements;
 
 public class UiManager : Singleton<UiManager>
 {
+	#region Input
+
+	private InputSystem_Actions inputActions;
+	private InputAction menuToggleAction;
+
+	private void OnEnable()
+	{
+		inputActions = new InputSystem_Actions();
+		menuToggleAction = inputActions.Player.MenuToggle;
+
+		inputActions.Enable();
+		menuToggleAction.performed += OnMenuToggle;
+	}
+
+	private void OnDisable()
+	{
+		menuToggleAction.performed -= OnMenuToggle;
+		inputActions.Disable();
+	}
+
+	private void OnMenuToggle(InputAction.CallbackContext context)
+	{
+		MenuToggle();
+	}
+
+	#endregion
+
 	// Editor Variables
 	[Header("References")]
 	public UIDocument gameUi;
@@ -21,5 +49,16 @@ public class UiManager : Singleton<UiManager>
 	public void BindEnemyStats(Stats stats)
 	{
 		gameUi.rootVisualElement.Q<ProgressBar>("EnemyHealth").dataSource = stats;
+	}
+
+	// Private Methods
+	private void MenuToggle()
+	{
+		statsUi.rootVisualElement.visible = !statsUi.rootVisualElement.visible;
+
+		if (statsUi.rootVisualElement.visible)
+			Time.timeScale = 0;
+		else
+			Time.timeScale = 1;
 	}
 }
