54629ab5710322a495ad953c97ffbbdf78ada2e1
Tian Bornman <tianbornman.tb@gmail.com>
2025-12-17 20:44:20 +0200

    Adding some new models

--- Assets/Scripts/Managers/SpawnManager.cs
+++ Assets/Scripts/Managers/SpawnManager.cs
@@ -2,7 +2,6 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using static UnityEditor.Experimental.AssetDatabaseExperimental.AssetDatabaseCounters;
 
 public class SpawnManager : Singleton<SpawnManager>
 {
